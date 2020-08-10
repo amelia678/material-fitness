e8e97d42280b17db0f32b2310e890a3a7252ab02
Amelia Schulz <amelia.schulz@gmail.com>
2020-08-10 17:24:28 -0400

    Add custom theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
 
-import { TextField, Typography } from "@material-ui/core";
+import { TextField, Typography, ThemeProvider } from "@material-ui/core";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <div className="App">
-     <Typography variant='h3' gutterBottom align='center'>Meditations</Typography>
-     <TextField></TextField>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography variant="h3" gutterBottom align="center">
+          Meditations
+        </Typography>
+        <TextField></TextField>
+        <Typography variant='body1'>here is some dummy text</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    h3: {
+      fontFamily: `"Montserrat", sans-serif`
+    },
+    body1: {
+      fontFamily: `"Montserrat", sans-serif`
+    }
+  }
+})
+
+
+export default theme
