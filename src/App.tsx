0c63ca144fbe333b4682e77a87c6605ea8a2cb6a
Amelia Schulz <amelia.schulz@gmail.com>
2020-08-10 18:42:02 -0400

    Add form field for meditations.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-import { TextField, Typography, ThemeProvider } from "@material-ui/core";
+import {
+  TextField,
+  Typography,
+  ThemeProvider,
+  FormGroup,
+  Container,
+} from "@material-ui/core";
 import theme from "./theme/theme";
 
 function App() {
+  const [meditation, setMeditation] = useState("");
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Typography variant="h3" gutterBottom align="center">
           Meditations
         </Typography>
-        <TextField></TextField>
-        <Typography variant='body1'>here is some dummy text</Typography>
+        <Container>
+          <form>
+            <FormGroup>
+              <TextField
+                name="meditation"
+                value={meditation}
+                margin="normal"
+                label="How long did you meditate?"
+                onChange={(e) => setMeditation((e.target.value))}
+              ></TextField>
+            </FormGroup>
+          </form>
+        </Container>
       </div>
     </ThemeProvider>
   );
