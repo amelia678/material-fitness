29e296581bf69d1f624f65f3939650fe7aea9959
Amelia Schulz <amelia.schulz@gmail.com>
2020-08-10 17:06:32 -0400

    Add typescript.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import { TextField, Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+     <Typography variant='h3' gutterBottom align='center'>Meditations</Typography>
+     <TextField></TextField>
+    </div>
+  );
+}
+
+export default App;
